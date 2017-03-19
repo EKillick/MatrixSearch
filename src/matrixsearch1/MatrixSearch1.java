dc49de6e788f7793fc41833fdf237e96d7b53c0a
EKillick <edwinkillick@gmail.com>
2017-03-19 16:53:36 +0000

    Testing code begun

--- src/matrixsearch1/MatrixSearch1.java
+++ src/matrixsearch1/MatrixSearch1.java
@@ -14,11 +14,13 @@ import java.util.Scanner;
 public class MatrixSearch1 {
 
     final static private boolean DEBUG = false;
+    final static private boolean TEST = false;
+    final static private boolean WORST = true;
 
     /**
      * A basic search method that searches an unsorted matrix linearly
-     *
      * @param num - number being searched for
+     * @param size - size of the array being searched
      * @param multi - array to be searched
      * @return - true if number is in matrix, false otherwise
      */
@@ -26,26 +28,24 @@ public class MatrixSearch1 {
         for (int row = 0; row < size; row++) {
             for (int col = 0; col < size; col++) {
                 if (num == multi[row][col]) {
-                    System.out.println(num + " was found in row: " + row + " col: " + col);
+//                    System.out.println(num + " was found in row: " + row + " col: " + col);
                     return true;
                 }
             }
 
         }
-        System.out.println(num + " was not found");
+//        System.out.println(num + " was not found");
         return false;
     }
 
     /**
      * A method that searches an array sorted by row but not by column
-     *
      * @param num - number being searched for
-     * @param size
+     * @param size - size of the array being searched
      * @param multi - array to be searched
      * @return - true if number is in matrix, false otherwise
      */
     public static boolean matrixSearch2(int[][] multi, int size, int num) {
-        // Start half way through the array
         int min;
         int max;
         int mid;
@@ -57,7 +57,7 @@ public class MatrixSearch1 {
                 while (min <= max) {
                     mid = ((max + min) / 2);
                     if (multi[row][mid] == num) {
-                        System.out.println(num + " was found in row: " + row + " col: " + mid);
+//                        System.out.println(num + " was found in row: " + row + " col: " + mid);
                         return true;
                     } else if (multi[row][mid] > num) {
                         max = mid - 1;
@@ -67,22 +67,22 @@ public class MatrixSearch1 {
                 }
             }
         }
-        System.out.println(num + " was not found");
+//        System.out.println(num + " was not found");
         return false;
     }
 
     /**
-     * @param num
-     * @param size
-     * @param multi
-     * @return
+     * @param num - number being searched for
+     * @param size - size of the array being searched
+     * @param multi - array to be searched
+     * @return - true if number is in matrix, false otherwise
      */
     public static boolean matrixSearch3(int[][] multi, int size, int num) {
         int col = size - 1;
         int row = 0;
         while (row < (size - 1) && col > 0) {
             if (multi[row][col] == num) {
-                System.out.println(num + " was found in row: " + row + " col: " + col);
+//                System.out.println(num + " was found in row: " + row + " col: " + col);
                 return true;
             } else if (multi[row][col] > num) {
                 col--;
@@ -90,8 +90,21 @@ public class MatrixSearch1 {
                 row++;
             }
         }
-        System.out.println(num + " was not found");
+//        System.out.println(num + " was not found");
         return false;
+    }
+    
+    private static int[][] generateMatrix(int n){
+        int[][] multi = new int[n][n];
+        int num = 1;
+        
+        for (int row = 0; row < n; row++){
+            for (int column = 0; column < n; column++){
+                multi[row][column] = num;
+                num++;
+            }
+        }
+        return multi;
     }
 
     public static void main(String[] args) {
@@ -104,11 +117,11 @@ public class MatrixSearch1 {
             {13, 15, 50, 100, 110, 112, 120},
             {22, 27, 61, 112, 119, 138, 153}};
         int[] testNumbers = new int[]{4, 12, 110, 5, 6, 111};
-        System.out.print("Question number: ");
-        Scanner scan = new Scanner(System.in);
-        int question = scan.nextInt();
-
+        
         if (DEBUG) {
+            System.out.print("Question number: ");
+            Scanner scan = new Scanner(System.in);
+            int question = scan.nextInt();
             System.out.print("Enter a number to search for: ");
             int num = scan.nextInt();
             switch (question) {
@@ -125,8 +138,11 @@ public class MatrixSearch1 {
                     break;
             }
         } 
-        else {
+        if (TEST) {
             for (int num : testNumbers) {
+                System.out.print("Question number: ");
+                Scanner scan = new Scanner(System.in);
+                int question = scan.nextInt();
                 switch (question) {
                     case 1:
                         System.out.println(matrixSearch1(multi, multi.length, num));
@@ -140,6 +156,33 @@ public class MatrixSearch1 {
                     default:
                         System.out.println("Enter 1, 2 or 3");
                 }
+            }
+        }
+        if (WORST){
+            int size[] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 200, 300, 
+                400, 500, 600, 700, 800, 900, 1000, 2000, 3000, 4000};
+            
+            for (int i = 0; i < size.length; i++) {
+                int n = size[i];
+                int num = 0;
+                int reps = 1000;
+                double sum = 0, s = 0;
+                double sumSquared = 0;
+                num = (n * n) + 1;
+                int matrix[][] = generateMatrix(n);
+
+                for (int j = 0; j < reps; j++) {
+                    long t1 = System.nanoTime();
+                    matrixSearch1(matrix, matrix.length, num);
+                    long t2 = System.nanoTime() - t1;
+                    sum += (double) t2 / 1000000.0;
+                    sumSquared += (t2 / 1000000.0) * (t2 / 1000000.0);
+                }
+                
+                double mean = sum / reps;
+                double variance = sumSquared / reps - (mean * mean);
+                double stdDev = Math.sqrt(variance);
+
             }
 
         }
